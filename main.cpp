10276c59e3047fbf8103f75e50d689c929298a89
attila3453 <attila_geiszt@yahoo.com>
2013-08-22 11:52:44 +0300

    Added colors

--- main.cpp
+++ main.cpp
@@ -116,12 +116,17 @@ int main() {
     window.setVerticalSyncEnabled(1);
     window.setActive(1);
 
-    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+    glClearColor( 0.5f, 0.5f, 0.5f, 1.0f );
 
     srand(time(0));
 
     glewExperimental = 1;
     glewInit();
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    GLuint programid = loadShaders("shader.vertex", "shader.fragment");
 
     GLuint triangleid;
     glGenVertexArrays(1, &triangleid);
@@ -133,14 +138,19 @@ int main() {
             0,   1, 0};
 
     GLuint vertexbuffer;
-
-    GLuint programid = loadShaders("shader.vertex", "shader.fragment");
-
-    glClearColor(0, 0, 1, 1);
-
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangledata), triangledata, GL_STATIC_DRAW);
+
+    GLfloat colordata[] = {
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1};
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colordata), colordata, GL_STATIC_DRAW);
 
     glm::mat4 projectionmat = glm::perspective(45.0f, (float)SW / (float)SH, 0.1f, 100.0f);
     glm::mat4 viewmat = glm::lookAt(glm::vec3(4.0f, 4.0f, 4.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
@@ -178,6 +188,10 @@ int main() {
         my = mpos.y;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
